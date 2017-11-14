7a008d2d817b43453e3528b9e9eef484643fb0e1
Alberto Perez <albertojperezf@gmail.com>
2017-11-14 17:27:49 +0100

    feat: add comment component to manage the comments of the app

---
+++ readable/src/components/Comment/index.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+// import classNames from 'classnames';
+import './styles.css';
+
+export default class Comment extends Component {
+    static propTypes = {
+        author: PropTypes.string,
+        body: PropTypes.string,
+        deleted: PropTypes.bool,
+        id: PropTypes.number,
+        parentDeleted: PropTypes.bool,
+        parentId: PropTypes.number,
+        timestamp: PropTypes.string,
+        voteScore: PropTypes.number
+    };
+
+    static defaultProps = {};
+
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        const { author, body, deleted, id, parentDeleted, parentId, timestamp, voteScore } = this.props
+        const {  } = this.state
+        return (
+            <div>
+                <p>
+                    I'm a comment
+                </p>
+            </div>
+        )
+    }
+}
