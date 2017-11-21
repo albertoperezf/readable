8dda0c4684b9c2b9bce792751a8644c9eabd763d
Alberto Perez <albertojperezf@gmail.com>
2017-11-21 16:21:02 +0100

    feat: add actions for the redux reducers of the posts

---
+++ readable/src/actions/posts.js
@@ -0,0 +1,53 @@
+export const ALL_POST = 'ALL_POST'
+export const ALL_POST_OF_CATEGORIES = 'ALL_POST_CATEGORIES'
+export const ADD_POST = 'ADD_POST'
+export const EDIT_POST = 'ADD_POST'
+export const DELETE_POST = 'DELETE_POST'
+export const VOTE_POST = 'ADD_POST'
+export const DETAILS_POST = 'DETAILS_POST'
+export const GET_COMMENT_POST = 'ADD_POST'
+export const ADD_COMMENT_POST = 'ADD_POST'
+
+export const allPost = () => ({
+    type: ALL_POST,
+});
+
+export const allPostOfCategories = category => ({
+    type: ALL_POST_OF_CATEGORIES,
+    category
+});
+
+export const addPost = id => ({
+    type: ADD_POST,
+    id
+});
+
+export const detailsPost = id => ({
+    type: DETAILS_POST,
+    id
+});
+
+export const votePost = id => ({
+    type: VOTE_POST,
+    id
+});
+
+export const editPost = id => ({
+    type: EDIT_POST,
+    id
+});
+
+export const deletePost = id => ({
+    type: DELETE_POST,
+    id
+});
+
+export const allCommentsPost = id => ({
+    type: GET_COMMENT_POST,
+    id
+});
+
+export const addCommentsPost = id => ({
+    type: ADD_COMMENT_POST,
+    id
+});
